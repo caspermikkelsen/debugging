74f08587e4f96d9db8699f034615f90d139dec4e
caspermikkelsen <caspermikkelsen06@gmail.com>
2023-11-14 12:10:06 +0100

    derere

--- assets/js/site.js
+++ assets/js/site.js
@@ -11,5 +11,13 @@ const myBooks = ["The Catcher in the Rye",
     "The Da Vinci Code",
     "The Hunger Games"]
 
-console.warn('Jeg er en advarsel!')
-console.error('Jeg er en fejl!!!')
+    console.clear();
+    console.group("konsol opgave");
+    console.log("Jeg er en almindelig konsol log");
+    console.info("Jeg er en konsol info:",myInfo);
+    console.warn("jeg er en advarsel!");
+    console.error("Jeg er en fejl!!!");
+    console.table(myBooks);
+    console.time();
+    console.timeLog();
+    console.groupEnd();
